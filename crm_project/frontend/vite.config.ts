0f2c6ff5e1fe4437fcbb0711c0cae9507057f973
Gmarques <gm.marques2010@gmail.com>
2025-03-28 23:38:04 +0000

    Add crm_project/frontend/vite.config.ts 1743205083712

---
+++ crm_project/frontend/vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    allowedHosts: ['h4pmlh-3001.csb.app'],
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+      }
+    }
+  }
+})
